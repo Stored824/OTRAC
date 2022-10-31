0bbce6c8b9dc87ebcf8e14f562735974dbbe3b8a
Andro Magaš <andro.magas@fer.hr>
2022-10-31 16:44:17 +0100

    Add postgre reading

---
+++ connection.js
@@ -0,0 +1,46 @@
+const { writeFileSync } = require('fs')
+const{Pool, Client} = require('pg')
+const fs = require("fs");
+
+const connectionString = 'postgressql://postgres:default@localhost:5432/TeamsAndPlayers'
+const client = new Client({
+    connectionString: connectionString
+
+})
+
+async function execute()
+{
+    client.connect()
+    // GET TEAMS
+    teams = await client.query('SELECT * FROM "Team"')
+    //console.log(teams.rows)
+    writeFileSync("teams.json",JSON.stringify(teams.rows))
+
+    // GET PLAYERS
+    players = await client.query('SELECT * FROM "Player" ORDER BY "Player"."current_team"')
+    //console.log(players.rows)
+    writeFileSync("players.json",JSON.stringify(players.rows))
+    
+     
+    client.end()
+
+}
+execute()
+
+teams = JSON.parse(fs.readFileSync('./teams.json','utf8'))
+players = JSON.parse(fs.readFileSync('./players.json','utf8'))
+
+teams.forEach(team => {
+    current_players = []
+    players.forEach(player =>
+    {
+        if(player['current_team'] == team['Team_ID'])
+        {
+            current_players.push(JSON.stringify(player))
+        }
+    })
+    team['players'] = current_players
+});
+
+writeFileSync("TeamsAndPlayers.json",JSON.stringify(teams))
+
